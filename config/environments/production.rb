5a7dfda8681e8d051c61bb7216f698f58f3704ed
Jarmo <jarmo@julianus.ee>
2017-09-08 12:36:43 +0300

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.scss']
+  config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
